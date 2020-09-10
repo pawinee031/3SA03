9d8a270c3e8b913cdec009bcea3ada3d429fc998
pawinee031 <s6135512031.phuket.psu.ac.th>
2020-09-10 12:20:05 +0700

    up

---
+++ 1/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CharacterCard from "./component/CharacterCard"
+import './App.css';
+
+const word = "Hello";
+function App() {
+ return (
+ <div>
+ {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ }
+ </div>
+ );
+}
+
+
+export default App;
